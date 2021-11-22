eefac29f97d882098ae73b06de0c430e10cc6088
Lucas Nell <lucas@lucasnell.com>
2021-11-22 06:28:38 -0600

    Put purge_dups back into pipeline, updated other numbers, added BRAKER2

---
+++ chtc/BRAKER2.sh
@@ -0,0 +1,8 @@
+
+# From Sean (19 Nov 2021)
+# Check the details on Braker2, but it might require or allow you to input
+# a transcriptome assembly, as well as used the raw reads.
+# Both might be helpful.
+
+
+
