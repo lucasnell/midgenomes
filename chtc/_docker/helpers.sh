5040a9f7ec5c9c8d77ea180722097cb903a5c9ba
Lucas Nell <lucas@lucasnell.com>
2022-06-07 16:08:39 -0500

    working docker file

--- chtc/_docker/helpers.sh
+++ chtc/_docker/helpers.sh
@@ -6,9 +6,12 @@
 
 
 #' Check previous command's exit status.
-#' If != 0, then archive current dir and exit.
-#' If the object `TARGET` is defined, it sends the tar file there.
-#' The tar file is sent to `/staging/lnell/` otherwise
+#' If != 0, then...
+#' ... non-interactive session: send error message, print any *.log or
+#'                              *.err files, and exit.
+#' ... interactive session: just break and ignore errors if the context
+#'                          isn't sensible.
+#'
 #' Usage:
 #' OperationX ... options ...
 #' check_exit_status "OperationX" $?
@@ -16,21 +19,22 @@
 check_exit_status () {
     if [ "$2" != "0" ]; then
         echo "Step $1 failed with exit status $2" 1>&2
-        # Only do exiting if inside a non-interactive job:
-        if [ -z "$PS1" ]; then
-            local wd=${PWD##*/}
-            cd ..
-            tar -czf ERROR_${wd}.tar.gz ${wd}
-            local SEND_LOC=/staging/lnell/
-            if [ -n "$TARGET" ]; then
-                SEND_LOC=${TARGET}
-            fi
-            mv ERROR_${wd}.tar.gz ${SEND_LOC}
-            rm -r ${wd}
-            exit $2
-        else
+        local LOGS=$(ls *.err *.out 2> /dev/null)
+        for f in $LOGS; do
+            echo -e "\n\nFILE: " ${f} "\n\n" 1>&2
+            cat ${f} 1>&2
+        done
+        out_loc=$(readlink -f /proc/self/fd/0)
+        if [ "$out_loc" != "/dev/null" ]; then
+            # (interactive shell)
             break 2> /dev/null
-            return 0
+        else
+            # (non-interactive shell)
+            cd $_CONDOR_SCRATCH_DIR
+            # Remove everything except for the files / folders you start with:
+            TO_RM=$(ls -I tmp -I var -I docker_stderror)
+            rm ${TO_RM} 2> /dev/null
+            exit $2
         fi
     fi
     if [[ "$1" != "null" ]]; then
