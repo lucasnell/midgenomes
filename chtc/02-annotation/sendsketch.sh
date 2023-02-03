b3fc573c4dd233a920aa45bebc5431b3737c89b1
Lucas Nell <lucas@lucasnell.com>
2023-02-03 11:00:44 -0800

    Added script to rename chironomid files, better sendsketch.sh descr.

--- chtc/02-annotation/sendsketch.sh
+++ chtc/02-annotation/sendsketch.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+#'
+#' One-time script (run interactively) using BBmap's sendsketch.sh to
+#' look for contamination in the Tgraci and Pstein assemblies.
+#'
+
 conda activate main-env
 
 
-export ASSEMBLY=tany_contigs.fasta.gz
+export ASSEMBLY=Tgraci_contigs.fasta.gz
 export ONT_READS=basecalls_guppy-5.0.11.fastq.gz
 
-cp /staging/lnell/assemblies/tany_contigs.fasta.gz ./
+cp /staging/lnell/assemblies/Tgraci_contigs.fasta.gz ./
 cp /staging/lnell/ont/basecalls_guppy-5.0.11.fastq.gz ./
 cp /staging/lnell/assemblies/Pstein_contigs.fasta.gz ./
 
 mkdir sendsketch
 
-sendsketch.sh in=tany_contigs.fasta.gz out=sendsketch/tany_contigs_sketch.out
+sendsketch.sh in=Tgraci_contigs.fasta.gz out=sendsketch/Tgraci_contigs_sketch.out
 
 sendsketch.sh in=basecalls_guppy-5.0.11.fastq.gz qin=33 reads=100k size=200k \
     out=sendsketch/ont_reads_sketch.out
