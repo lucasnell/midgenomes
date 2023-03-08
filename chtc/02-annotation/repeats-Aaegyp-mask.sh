d32b8de56030bf237992637a8894c0e36b4a3c5d
Lucas Nell <lucas@lucasnell.com>
2023-03-08 11:20:43 -0800

    Added files to separate model and mask steps for Aedes aegypti
    This was because it took longer than the 72 hour limit on CHTC

---
+++ chtc/02-annotation/repeats-Aaegyp-mask.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+#'
+#' Use RepeatMasker to count repeat elements and to softmask the
+#' Aedes aegypti assembly.
+#'
+
+
+
+
+
+
+#' ===========================================================================
+#' ===========================================================================
+#'
+#' Read inputs
+#'
+#' ===========================================================================
+#' ===========================================================================
+
+
+export OUTPUT_LOC=/staging/lnell/annotations
+export OUT_PREFIX=Aaegyp
+export ASSEMBLY=Aaegyp_contigs.fasta
+
+if [ ! -d "${OUTPUT_LOC}" ]; then
+    echo "ERROR: '${OUTPUT_LOC}' does not exist." 1>&2
+    exit 1
+fi
+
+
+
+
+
+#' ===========================================================================
+#' ===========================================================================
+#'
+#' Basics to start out
+#'
+#' ===========================================================================
+#' ===========================================================================
+
+
+
+#' This should be divisible by 4 bc that's how many threads are used per
+#' job using RMBlast, the default engine in RepeatModeler
+export THREADS=$(grep "^Cpus = " $_CONDOR_MACHINE_AD | sed 's/Cpus\ =\ //')
+if (( $THREADS < 4 )); then
+    echo "ERROR: You need >= 4 threads, but you provided $THREADS." 1>&2
+    exit 1
+fi
+
+. /app/.bashrc
+conda activate repeat-env
+
+
+export OUT_DIR=${OUT_PREFIX}_repeats
+# mkdir ${OUT_DIR}
+# cd ${OUT_DIR}
+
+
+#' Naming outputs:
+#' ------
+#' Library of repeats from RepeatModeler:
+export REPEATS_LIB=${OUT_PREFIX}_repeats_lib.fasta
+#' Soft-masked assembly for use in BRAKER:
+export MASKED_ASSEMBLY=${OUT_PREFIX}_contigs_masked.fasta
+#' GFF of repeats in assembly:
+export REPEAT_LOCS=${OUT_PREFIX}_repeats_locs.gff3
+
+
+
+
+#' ------------------------------------------------
+#' Output from previous RepeatModeler step
+#' ------------------------------------------------
+
+
+if [ ! -f "${OUTPUT_LOC}/${OUT_DIR}_model.tar.gz" ]; then
+    echo "ERROR: '${OUTPUT_LOC}/${OUT_DIR}_model.tar.gz' does not exist." 1>&2
+    exit 1
+fi
+tar -xf ${OUTPUT_LOC}/${OUT_DIR}_model.tar.gz -C ./
+if [ ! -d "${OUT_DIR}_model" ]; then
+    echo -n "ERROR: '${OUT_DIR}_model' does not exist inside " 1>&2
+    echo "'${OUTPUT_LOC}/${OUT_DIR}_model.tar.gz'." 1>&2
+    exit 1
+fi
+mv ${OUT_DIR}_model ${OUT_DIR}
+cd ${OUT_DIR}
+
+
+
+
+
+
+#' ------------------------------------------------
+#' classify more elements using TEclass
+#' ------------------------------------------------
+
+
+mkdir teclass
+cd teclass
+
+
+#' Name of output from TEclass:
+export REPEATS_LIB_TEC=${OUT_PREFIX}_repeats_lib_TEclass.fasta
+#' Name for the RepeatModeler library (it's renamed here):
+export REPEATS_LIB_RM=${OUT_PREFIX}_repeats_lib_RM.fasta
+#' Combined output from TEclass and RepeatModeler (this is the same as
+#' what will later be $REPEATS_LIB):
+export REPEATS_LIB_COMBINED=${OUT_PREFIX}_repeats_lib_combined.fasta
+
+cp ../modeler/${OUT_PREFIX}-families.fa ${REPEATS_LIB_RM}
+
+#' Split the repeat-element library into known (IDed by RepeatModeler)
+#' and unknown elements.
+conda activate main-env
+cat "${REPEATS_LIB_RM}" \
+    | seqkit fx2tab \
+    | grep -v "#Unknown" \
+    | seqkit tab2fx \
+    | seqtk seq -l 50 - \
+    > "${REPEATS_LIB_RM%.fasta}_known.fasta"
+check_exit_status "known split" $?
+cat "${REPEATS_LIB_RM}" \
+    | seqkit fx2tab \
+    | grep "#Unknown" \
+    | seqkit tab2fx \
+    | seqtk seq -l 50 - \
+    > "${REPEATS_LIB_RM%.fasta}_unknown.fasta"
+check_exit_status "unknown split" $?
+conda deactivate
+
+
+conda activate teclass-env
+TEclassTest.pl -r -o tec_out "${REPEATS_LIB_RM%.fasta}_unknown.fasta"
+conda deactivate
+
+
+cd tec_out
+cp ${REPEATS_LIB_RM%.fasta}_unknown.fasta.lib ../${REPEATS_LIB_TEC}
+mv ${REPEATS_LIB_RM%.fasta}_unknown.fasta.html \
+    ${REPEATS_LIB_RM%.fasta}_unknown.fasta.lib  \
+    ${REPEATS_LIB_RM%.fasta}_unknown.fasta.stat ../
+cd ..
+rm -r tec_out
+
+
+
+# Convert TEclass output to FASTA for input to RepeatMasker
+python3 << EOF
+
+import sys
+import re
+
+# maps TEclass classes into RepeatMasker classes.
+# Unfortunately, nonLTR and Retro classes in TEclass don't have parallels (to
+# my knowledge) in RepeatMasker so are called Unknown.
+mapper = {"DNA": "DNA", "LINE": "LINE", "LTR": "LTR", "nonLTR": "Unknown",
+          "Retro": "Unknown", "SINE": "SINE", "unclear": "Unknown"}
+
+tec_fasta = "${REPEATS_LIB_TEC}"
+rm_fasta  = "${REPEATS_LIB_RM%.fasta}_known.fasta"
+out_fasta = "${REPEATS_LIB_COMBINED}"
+
+if __name__ == "__main__":
+    with open(out_fasta, "wt") as out_file:
+        with open(tec_fasta, "rt") as tec_file:
+            for line in tec_file:
+                if line.startswith(">"):
+                    header_list = re.sub(r"\|.*|Unknown.*TEclass result: ", "",
+                                         line.rstrip()).split("#")
+                    header_list[1] = mapper[header_list[1]]
+                    new_header = "#".join(header_list) + "\n"
+                    b = out_file.write(new_header)
+                else:
+                    b = out_file.write(line)
+        with open(rm_fasta, "rt") as rm_file:
+            for line in rm_file:
+                if line.startswith(">"):
+                    new_header = re.sub(" .*", "",  line)
+                    b = out_file.write(new_header)
+                else:
+                    b = out_file.write(line)
+    sys.exit(0)
+EOF
+
+
+cp ${REPEATS_LIB_COMBINED} ../${REPEATS_LIB}
+
+
+
+cd ..
+
+
+
+
+
+#' ------------------------------------------------
+#' softmask assembly using RepeatMasker
+#' ------------------------------------------------
+
+mkdir masker
+cd masker
+
+mv ../modeler/${ASSEMBLY} ./
+cp ../${REPEATS_LIB} ./
+
+
+orig_wd=$(pwd)
+#' Get Diptera repeats from RepeatMasker library that includes RepBase
+#' RepeatMasker Edition:
+cd /opt/conda/envs/repeat-env/share/RepeatMasker
+./famdb.py -i ./Libraries/RepeatMaskerLib.h5 families --format fasta_name \
+    --include-class-in-name --ancestors --descendants 'diptera' \
+    > ${orig_wd}/diptera_and_${REPEATS_LIB}
+cd ${orig_wd}
+
+cat ${REPEATS_LIB} \
+    >> diptera_and_${REPEATS_LIB}
+
+
+RepeatMasker \
+    -s -xsmall -gff \
+    -lib diptera_and_${REPEATS_LIB} \
+    -pa $(( THREADS / 4 )) \
+    ${ASSEMBLY} \
+    1> >(tee -a RepeatMasker.out)
+
+
+cp ${ASSEMBLY}.masked ../${MASKED_ASSEMBLY}
+cp ${ASSEMBLY}.out.gff ../${REPEAT_LOCS}
+rm ${REPEATS_LIB} ${ASSEMBLY}
+cd ..
+
+
+
+
+
+#' ------------------------------------------------
+#' handling final output
+#' ------------------------------------------------
+
+
+gzip < ${MASKED_ASSEMBLY} > ${MASKED_ASSEMBLY}.gz
+mv ${MASKED_ASSEMBLY}.gz "${OUTPUT_LOC}"/
+
+cd ..
+tar -czf ${OUT_DIR}.tar.gz ${OUT_DIR}
+mv ${OUT_DIR}.tar.gz "${OUTPUT_LOC}"/
+rm -r ${OUT_DIR}
+
+
+
+
---
+++ chtc/02-annotation/repeats-Aaegyp-model.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+
+#' Use RepeatModeler to create a repeat library for the Aedes aegypti assembly.
+#'
+
+
+time_stamp () {
+    date +'%e %b %Y%t%T%n'
+}
+export progress_file=/staging/lnell/repeats-Aaegyp-progress.txt
+echo -e "\n\n" > $progress_file
+echo "-----------------------------------------------------------------------" \
+    >> $progress_file
+echo -e "\n\n" >> $progress_file
+echo "           START RUN" >> $progress_file
+time_stamp >> $progress_file
+echo -e "\n\n" >> $progress_file
+echo "-----------------------------------------------------------------------" \
+    >> $progress_file
+echo -e "\n\n" >> $progress_file
+
+
+
+#' ===========================================================================
+#' ===========================================================================
+#'
+#' Read inputs
+#'
+#' ===========================================================================
+#' ===========================================================================
+
+
+export OUTPUT_LOC=/staging/lnell/annotations
+
+export OUT_PREFIX=Aaegyp
+
+export ASSEMBLY_FULL_PATH=/staging/lnell/assemblies/Aaegyp_contigs.fasta.gz
+
+
+if [ ! -f "${ASSEMBLY_FULL_PATH}" ]; then
+    echo "ERROR: Your assembly file ('${ASSEMBLY_FULL_PATH}') does not exist." 1>&2
+    exit 1
+fi
+if ! [[ "${ASSEMBLY_FULL_PATH}" =~ (.fasta|.fa|.fasta.gz|.fa.gz)$ ]]; then
+    echo -n "ERROR: Assembly must end in '.fasta', '.fa', '.fasta.gz', or '.fa.gz'. " 1>&2
+    echo "Yours is '${ASSEMBLY_FULL_PATH}'." 1>&2
+    exit 1
+fi
+
+if [ ! -d "${OUTPUT_LOC}" ]; then
+    echo "ERROR: '${OUTPUT_LOC}' does not exist." 1>&2
+    exit 1
+fi
+
+
+
+
+
+#' ===========================================================================
+#' ===========================================================================
+#'
+#' Basics to start out
+#'
+#' ===========================================================================
+#' ===========================================================================
+
+
+
+#' This should be divisible by 4 bc that's how many threads are used per
+#' job using RMBlast, the default engine in RepeatModeler
+export THREADS=$(grep "^Cpus = " $_CONDOR_MACHINE_AD | sed 's/Cpus\ =\ //')
+if (( $THREADS < 4 )); then
+    echo "ERROR: You need >= 4 threads, but you provided $THREADS." 1>&2
+    exit 1
+fi
+
+. /app/.bashrc
+conda activate repeat-env
+
+
+export OUT_DIR=${OUT_PREFIX}_repeats_model
+mkdir ${OUT_DIR}
+cd ${OUT_DIR}
+
+
+#' Naming outputs:
+#' ------
+#' Library of repeats from RepeatModeler:
+export REPEATS_LIB=${OUT_PREFIX}_repeats_lib.fasta
+#' Soft-masked assembly for use in BRAKER:
+export MASKED_ASSEMBLY=${OUT_PREFIX}_contigs_masked.fasta
+#' GFF of repeats in assembly:
+export REPEAT_LOCS=${OUT_PREFIX}_repeats_locs.gff3
+
+
+cp ${ASSEMBLY_FULL_PATH} ./
+check_exit_status "cp genome" $?
+export ASSEMBLY=$(basename "${ASSEMBLY_FULL_PATH}")
+if [[ "${ASSEMBLY}" == *.gz ]]; then
+    gunzip ${ASSEMBLY}
+    check_exit_status "gunzip genome" $?
+    ASSEMBLY=${ASSEMBLY%.gz}
+fi
+
+
+
+#' ------------------------------------------------
+#' prep for RepeatModeler
+#' ------------------------------------------------
+
+conda activate main-env
+
+
+mv ${ASSEMBLY} ${ASSEMBLY%.*}_orig.fasta
+check_exit_status "rename genome" $?
+
+#' Below does the following (in order):
+#' - Simplify sequence names in headers (remove all after first space)
+#' - Undo softmasking (I'll be manually softmasking repeat regions, if desired, later)
+#' - Change to narrow (i.e., multi-line) format
+sed '/^>/ s/ .*//' ${ASSEMBLY%.*}_orig.fasta \
+    | tr [:lower:] [:upper:] \
+    | seqtk seq -l 60 - \
+    > ${ASSEMBLY}
+rm ${ASSEMBLY%.*}_orig.fasta
+
+conda deactivate
+
+
+echo "finished RM prep" >> $progress_file
+time_stamp >> $progress_file
+
+
+#' ------------------------------------------------
+#' run RepeatModeler
+#' ------------------------------------------------
+
+conda activate repeat-env
+
+
+mkdir modeler
+cd modeler
+mv ../${ASSEMBLY} ./
+
+# (RMBlast is default but put here for clarity)
+BuildDatabase -engine rmblast -name ${OUT_PREFIX} ${ASSEMBLY} \
+    >& BuildDatabase.log
+check_exit_status "BuildDatabase" $?
+
+echo "finished BuildDatabase" >> $progress_file
+time_stamp >> $progress_file
+
+
+echo "" > /staging/lnell/repeats-Aaegyp-RepeatModeler.out
+echo "" > /staging/lnell/repeats-Aaegyp-RepeatModeler.err
+
+
+timeout 65h \
+    RepeatModeler -database ${OUT_PREFIX} -pa $(( THREADS / 4 )) -LTRStruct \
+    -srand 633659157 \
+    1> >(tee -a /staging/lnell/repeats-Aaegyp-RepeatModeler.out) \
+    2> >(tee -a /staging/lnell/repeats-Aaegyp-RepeatModeler.err)
+timeout_exit_status=$?
+# check_exit_status "RepeatModeler" $?
+
+cp /staging/lnell/repeats-Aaegyp-RepeatModeler.out RepeatModeler.out
+
+cd ..
+
+
+# Code 124 means it was still running
+if [ $timeout_exit_status -eq 124 ]; then
+    echo "ended RepeatModeler early" >> $progress_file
+    time_stamp >> $progress_file
+    cd ..
+    tar -czf ${OUT_DIR}-unfinished.tar.gz ${OUT_DIR}
+    mv ${OUT_DIR}-unfinished.tar.gz "${OUTPUT_LOC}"/
+    rm -r ${OUT_DIR}
+    # exit 85
+    exit 1
+fi
+
+
+
+echo "finished RepeatModeler" >> $progress_file
+time_stamp >> $progress_file
+
+
+
+
+
+#' ------------------------------------------------
+#' handling final output
+#' ------------------------------------------------
+
+
+# gzip < ${MASKED_ASSEMBLY} > ${MASKED_ASSEMBLY}.gz
+# mv ${MASKED_ASSEMBLY}.gz "${OUTPUT_LOC}"/
+
+cd ..
+tar -czf ${OUT_DIR}.tar.gz ${OUT_DIR}
+mv ${OUT_DIR}.tar.gz "${OUTPUT_LOC}"/
+rm -r ${OUT_DIR}
+
+
+
+exit 0
