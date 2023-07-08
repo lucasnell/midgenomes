51c70b027206b2a2ff6798bde81346f9a3e0cc9a
Lucas Nell <lucnell@gmail.com>
2023-07-08 09:40:47 -0700

    hyphy-genes.R outputs gene names and HOGs bc codon aligner required

--- hyphy-genes.R
+++ hyphy-genes.R
@@ -1,9 +1,34 @@
 
-library(GO.db)
-# ^^ make sure this is loaded before tidyverse ^^
-library(tidyverse)
-library(parallel)
+suppressPackageStartupMessages({
+    library(GO.db)
+    # ^^ make sure this is loaded before tidyverse ^^
+    library(tidyverse)
+    library(parallel)
+})
 options("mc.cores" = max(1L, parallel::detectCores()-2L))
+
+
+read_fasta <- function(fn) {
+    z <- read_lines(fn, progress = FALSE)
+    heads <- which(grepl("^>", z))
+    nh <- length(heads)
+    sl <- c(map(2:nh, \(i) (heads[i-1]+1L):(heads[i]-1L)),
+            list((heads[nh]+1L):length(z)))
+    zz <- map_chr(sl, \(inds) paste0(z[inds], collapse = ""))
+    names(zz) <- z[heads]
+    return(zz)
+}
+write_faa <- function(faa_obj, fn) {
+    stopifnot(inherits(faa_obj, "character"))
+    stopifnot(!is.null(names(faa_obj)))
+    stopifnot(all(grepl("^>", names(faa_obj))))
+    file_conn <- file(fn, "at")
+    for (i in 1:length(faa_obj)) {
+        writeLines(c(names(faa_obj)[i], faa_obj[[i]]), file_conn)
+    }
+    close(file_conn)
+    invisible(NULL)
+}
 
 
 hog_gos <- "~/_data/chir_orthofinder/orthofinder-output/Single_Copy_HOG_GO/N0-GO-by-HOG.tsv" |>
@@ -11,7 +36,10 @@ hog_gos <- "~/_data/chir_orthofinder/orthofinder-output/Single_Copy_HOG_GO/N0-GO
     mutate(go = go |>
                toupper() |>
                str_split(";")) |>
-    unnest(go)
+    unnest(go) |>
+    select(-node)
+
+
 
 
 #' Get a GO term plus all its offspring. Can optionally be done recursively.
@@ -60,25 +88,20 @@ get_term <- function(gos) {
 #'   * GO:0010212 - response to ionizing radiation
 #'
 #'
-#' Within response to stimulus > response to stress >
-#' cellular response to stress:
-#'
-#'   * GO:0006281 - DNA repair (in DNA damage response)
-#'   * GO:0042262 - DNA protection
-#'
-#'
 #' Within response to stimulus > response to stress:
 #'
 #'   * GO:0034059 - response to anoxia
-#'   * GO:0061771 - response to caloric restriction
 #'   * GO:0009409 - response to cold
 #'   * GO:0009408 - response to heat
-#'   * GO:0090664 - response to high population density
 #'   * GO:0001666 - response to hypoxia
 #'   * GO:0006979 - response to oxidative stress
-#'   * GO:0042594 - response to starvation
-
-
+#'
+#'
+#' Within response to stimulus > response to external stimulus > response to
+#' stress > response to other organism:
+#'
+#'   * GO:0098542 defense response to other organism
+#'
 
 
 #' Dataframe of GO terms that I'm interested in analyzing.
@@ -86,18 +109,14 @@ get_term <- function(gos) {
 #' (recursively) for these terms.
 #'
 
-focal_go_df <- tibble(go = c("GO:0010038", "GO:0006281", "GO:0042262",
-                             "GO:0010212", "GO:0034059", "GO:0061771",
-                             "GO:0009409", "GO:0009408", "GO:0090664",
-                             "GO:0001666", "GO:0006979", "GO:0042594"),
+focal_go_df <- tibble(go = c("GO:0010038", "GO:0010212", "GO:0034059",
+                             "GO:0009409", "GO:0009408", "GO:0001666",
+                             "GO:0006979", "GO:0098542"),
                       term = get_term(go),
                       # parent GO term plus offspring (recursive)
                       offspring = mclapply(go, get_offs),
                       hogs = map(offspring, \(o) hog_gos$hog[hog_gos$go %in% o]))
 focal_go_df
-
-#' Some have zero HOGs associated with them, so we can filter these out:
-focal_go_df <- filter(focal_go_df, map_int(hogs, length) > 0)
 
 
 #' Below shows that some HOGs are shared among the GO terms:
@@ -113,19 +132,30 @@ sum(shared_mat[upper.tri(shared_mat)] > 0)
 #' Whole matrix with total HOGs on diagonal:
 shared_mat + diag(map_int(focal_go_df$hogs, length))
 shared_mat
-#' Heatmap of shared HOGs:
-heatmap(shared_mat, Rowv = NA, Colv = NA, symm = FALSE)
 
 
-#' Extract sequences for all HOGs:
+#' Extract names of genes and species for all HOGs we want to analyze.
+#' We will have to use CDS in HyPhy, and we'll extract these in the bash
+#' script for HyPhy.
+#' We can't simply use the protein sequences because, according to
+#' a HyPhy developer:
+#'   > In order for our tools to run on sequence data, the sequences must be
+#'   > in the format of a codon-aware sequence alignment. Therefore the
+#'   > sequences must be divisible by three, in codons.
+#'   source: https://github.com/veg/hyphy/issues/1174#issuecomment-648068105
 
-all_hogs <- do.call(c, focal_go_df$hogs)
-hog_in <- \(x) {
-    paste0("~/_data/chir_orthofinder/orthofinder-output/",
-           "Single_Copy_HOG_Sequences/N0/", x, ".faa")
-}
 
-for (h in all_hogs[1:5]) {
-    hog_in(h)
-}
 
+all_hogs <- focal_go_df$hogs |>
+    do.call(what = c) |>
+    unique()
+
+hog_genes <- paste0("~/_data/chir_orthofinder/orthofinder-output/",
+                    "Single_Copy_HOG_GO/N0-GO-by-species-genes.tsv") |>
+    read_tsv(col_types = cols()) |>
+    select(-node, -go) |>
+    filter(hog %in% all_hogs) |>
+    arrange(species, hog, gene) |>
+    select(species, hog, gene)
+
+write_csv(hog_genes, "_data/hyphy-hog-genes.csv.gz")
