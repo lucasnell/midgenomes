7652353887ca8266d1639918b06f337b2dfabe98
Lucas Nell <lucnell@gmail.com>
2023-11-06 15:58:55 -0800

    Made summary tables for HyPhy

--- hyphy-summarize.R
+++ hyphy-summarize.R
@@ -2,12 +2,12 @@
 source("_scripts/00-preamble.R")
 
 library(jsonlite)
+library(gt)
+library(knitr)
 
+# To have tibbles display more digits:
+options(pillar.sigfig = 4)
 
-# LEFT OFF ----
-#' How to summarize this information?
-#' Are these tests redundant with each other?
-#'
 
 
 #' Benjamini–Yekutieli procedure for adjusting for multiple tests
@@ -15,7 +15,7 @@ library(jsonlite)
 #' Returns a logical vector for whether each p-value represents an actual
 #' discovery.
 #'
-BY_correct <- function(Pvals, .names, fdr = 0.05) {
+BY_correct <- function(Pvals, .names, fdr = 0.1) {
     m <- length(Pvals)
     cm <- sum(1 / (1:m))
     names(Pvals) <- .names
@@ -48,8 +48,8 @@ all_relax_data <- list.files(relax(""), "*.json") |>
 names(all_busted_data) <- list.files(busted(""), "*.json") |> str_remove(".json$")
 names(all_relax_data) <- list.files(relax(""), "*.json") |> str_remove(".json$")
 
-str(all_busted_data[[1]], max.level = 1)
-str(all_relax_data[[1]], max.level = 1)
+# str(all_busted_data[[1]], max.level = 1)
+# str(all_relax_data[[1]], max.level = 1)
 
 
 #' The same 12 HOGs failed for both. No idea why.
@@ -66,6 +66,7 @@ relax_data <- all_relax_data[map_lgl(all_relax_data, \(x) !isTRUE(is.na(x)))]
 
 # These objects take up a lot of memory, so remove them from environment:
 rm(all_busted_data, all_relax_data); invisible(gc())
+
 
 
 hyphy_df <- tibble(hog = names(busted_data),
@@ -93,30 +94,156 @@ sum(hyphy_df$busted_disc & hyphy_df$relax_disc & hyphy_df$relax_k > 1)
 sum(hyphy_df$busted_disc & hyphy_df$relax_disc & hyphy_df$relax_k < 1)
 
 
+# hist(hyphy_df$busted_pvals)
+# hist(hyphy_df$relax_pvals)
 
 
-hist(hyphy_df$busted_pvals)
-hist(hyphy_df$relax_pvals)
+
+hog_focal_go_df <- "_data/hyphy-focal-hog-go.csv" |>
+    read_csv(col_types = cols()) |>
+    select(-offspring) |>
+    rename(hog = hogs) |>
+    mutate(hog = str_split(hog, ";")) |>
+    unnest(hog) |>
+    filter(hog %in% hyphy_df$hog) |>
+    mutate(busted_disc = map_lgl(hog, \(h) hyphy_df$busted_disc[hyphy_df$hog == h]),
+           relax_disc = map_lgl(hog, \(h) hyphy_df$relax_disc[hyphy_df$hog == h]))
+
+
+sign_hogs <- hog_focal_go_df |>
+    filter(busted_disc & relax_disc) |>
+    getElement("hog") |>
+    unique()
+
+
+hog_focal_go_df |>
+    group_by(go, term) |>
+    rename(GO = go, Description = term) |>
+    summarize(n = n(),
+              BUSTED = mean(busted_disc) * 100,
+              RELAX = mean(relax_disc) * 100,
+              Both = mean(busted_disc & relax_disc) * 100,
+              .groups = "drop") |>
+    gt() |>
+    cols_label(n = "{{*n*}}") |>
+    fmt_number(BUSTED:Both, decimals = 1) |>
+    tab_spanner(label = "% significant", columns = BUSTED:Both) |>
+    fmt_integer(n) |>
+    opt_table_font(font = "Helvetica") |>
+    tab_style(style = list(cell_text(align = "center")),
+              locations = cells_column_labels(-Description)) |>
+    tab_options(data_row.padding = 1.5,
+                column_labels.padding = 1.5,
+                table.font.size = 10,
+                column_labels.border.top.width = 2,
+                column_labels.border.top.color = "black",
+                column_labels.border.bottom.width = 1,
+                column_labels.border.bottom.color = "black",
+                table_body.border.top.width = 1,
+                table_body.border.top.color = "black",
+                table_body.border.bottom.width = 2,
+                table_body.border.bottom.color = "black",
+                table_body.hlines.width = 0,
+                table.font.color = "black") |>
+    gtsave("_figures/hyphy-go-sign-percents.pdf")
+
+plot_crop("_figures/hyphy-go-sign-percents.pdf", quiet = TRUE)
+
+
+
 
 
 hyphy_df |>
-    # filter(relax_k > 1) |>
-    ggplot(aes(relax_k, - log10(relax_pvals))) +
-    geom_point()
+    filter(hog %in% sign_hogs) |>
+    select(1:4) |>
+    mutate(desc = case_when(hog == "N0.HOG0006267" ~ "exportin",
+                            hog == "N0.HOG0001010" ~ "peroxiredoxin-1",
+                            hog == "N0.HOG0005696" ~ "sulfonylurea receptor",
+                            hog == "N0.HOG0007992" ~ "F-box and wd40 domain protein 7",
+                            hog == "N0.HOG0007399" ~ "Serrate protein",
+                            hog == "N0.HOG0007118" ~ "dynein heavy chain",
+                            hog == "N0.HOG0007838" ~ "prohibitin",
+                            hog == "N0.HOG0008783" ~ "protein kinase C",
+                            hog == "N0.HOG0001074" ~ "ribosomal protein S6 kinase",
+                            hog == "N0.HOG0004934" ~ "4-aminobutyrate aminotransferase",
+                            hog == "N0.HOG0006858" ~ "Vav guanine nucleotide exchange factor",
+                            hog == "N0.HOG0006910" ~ "ETS domain-containing protein",
+                            hog == "N0.HOG0007233" ~ "Heat shock protein 83",
+                            hog == "N0.HOG0007347" ~ "Cyclic-nucleotide-gated cation channel",
+                            TRUE ~ NA)) |>
+    select(hog, desc, everything()) |>
+    gt() |>
+    cols_label(hog = "HOG",
+               desc = "Description",
+               busted_pvals = "{{*P*-value}}",
+               relax_pvals = "{{*P*-value}}",
+               relax_k = "{{*k*}}") |>
+    fmt_scientific(contains("pvals"), decimals = 2) |>
+    fmt_number(relax_k, decimals = 2) |>
+    tab_spanner(label = "BUSTED", columns = contains("busted")) |>
+    tab_spanner(label = "RELAX", columns = contains("relax")) |>
+    opt_table_font(font = "Helvetica") |>
+    tab_style(style = list(cell_text(align = "center")),
+              locations = cells_column_labels(-desc)) |>
+    tab_options(data_row.padding = 1.5,
+                column_labels.padding = 1.5,
+                table.font.size = 10,
+                column_labels.border.top.width = 2,
+                column_labels.border.top.color = "black",
+                column_labels.border.bottom.width = 1,
+                column_labels.border.bottom.color = "black",
+                table_body.border.top.width = 1,
+                table_body.border.top.color = "black",
+                table_body.border.bottom.width = 2,
+                table_body.border.bottom.color = "black",
+                table_body.hlines.width = 0,
+                table.font.color = "black") |>
+    gtsave("_figures/hyphy-sign-hogs.pdf")
+
+plot_crop("_figures/hyphy-sign-hogs.pdf", quiet = TRUE)
 
 
-hyphy_df |>
-    mutate(hog = factor(hog) |> fct_reorder(busted_pvals)) |>
-    select(hog, busted_pvals, relax_pvals) |>
-    ggplot(aes(hog, busted_pvals)) +
-    geom_point() +
-    coord_flip()
 
-hyphy_df |>
-    mutate(hog = factor(hog) |> fct_reorder(relax_pvals)) |>
-    select(hog, busted_pvals, relax_pvals) |>
-    ggplot(aes(hog, relax_pvals)) +
-    geom_point() +
-    coord_flip()
 
+
+# This helped me manually describe each HOG:
+
+
+hogs <- c("N0.HOG0001074", "N0.HOG0004934", "N0.HOG0006858", "N0.HOG0006910", "N0.HOG0007233", "N0.HOG0007347")
+
+sp <- "Cquinq"
+
+
+genes <- "_data/hyphy-hog-genes.csv" |>
+    read_csv(col_types = cols()) |>
+    filter(species == sp, hog %in% hogs) |>
+    mutate(hog = factor(hog, levels = hogs)) |>
+    arrange(hog) |>
+    getElement("gene")
+
+paste0(dirs$features, "/", sp, "_features.gff3.gz") |>
+    read_tsv(comment = "#",
+             col_names = c("seqid", "source", "type", "start",
+                           "end", "score", "strand", "phase",
+                           "attributes"),
+             col_types = "ccciicccc", progress = FALSE) |>
+    filter(type == "protein_coding_gene") |>
+    select(attributes) |>
+    mutate(gene = str_remove(attributes, "ID=") |>
+               str_remove(";.*"),
+           desc = str_remove(attributes, ".*description=") |>
+               str_remove(";.*")) |>
+    filter(gene %in% genes) |>
+    mutate(gene = factor(gene, levels = genes)) |>
+    arrange(gene) |>
+    select(gene, desc)
+
+
+"_data/hyphy-hog-genes.csv" |>
+    read_csv(col_types = cols()) |>
+    filter(species == sp, hog %in% hogs) |>
+    mutate(hog = factor(hog, levels = hogs)) |>
+    arrange(hog) |>
+    getElement("gene") |>
+    paste(collapse = "\n") |> cat()
 
