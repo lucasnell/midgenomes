030fe1a8e707b67da5bd6884758f49d09c0e0410
Lucas Nell <lucnell@gmail.com>
2024-03-27 10:26:56 -0700

    raxml bootstrapping (all reps) + support is all in one job

--- _bash/03-phylo/raxml-boot.sh
+++ _bash/03-phylo/raxml-boot.sh
@@ -2,28 +2,9 @@
 
 
 #'
-#' Run 100 bootstrap replicates using RAxML-NG.
-#' It requires as input an index for which seed to use (0-9).
-#' This allows bootstrapping to be split among multiple smaller jobs to be
-#' combined later.
+#' Run up to 1000 bootstrap replicates using RAxML-NG and compute
+#' branch support.
 #'
-
-
-if (( $# != 1 )); then
-    echo -e "ERROR: raxml-boot.sh requires exactly 1 argument" 1>&2
-    exit 1
-fi
-export SEEDS_IND=$1
-if ! [[ $SEEDS_IND =~ ^[0-9]+$ ]] || (( SEEDS_IND < 0 )) || (( SEEDS_IND > 9 )); then
-    echo -n "ERROR: arg to raxml-boot.sh should be an integer 0-9. " 1>&2
-    echo "Yours is '${SEEDS_IND}'." 1>&2
-    exit 2
-fi
-#' Array of seeds (previously generated using `sample.int(2^31-1, 10)` in R):
-SEEDS_ARR=(1478865727 158688736 548184584 1400912585 357834478 667771158
-           145164412 1949248987 532694851 634149308)
-
-export THIS_SEED=${SEEDS_ARR[$SEEDS_IND]}
 
 
 . /app/.bashrc
@@ -33,7 +14,7 @@ export THREADS=$(count_threads)
 
 export TARGET=/staging/lnell/phylo
 
-export OUT_DIR=chir_raxml_boot_${SEEDS_IND}
+export OUT_DIR=chir_raxml_boot
 export PREFIX=chir_phy
 mkdir ${OUT_DIR}
 cd ${OUT_DIR}
@@ -41,64 +22,96 @@ cd ${OUT_DIR}
 
 
 export CONCAT_ALIGNS=mafft_aligns_concat.faa
-cp ${TARGET}/${CONCAT_ALIGNS}.gz ./ && gunzip ${CONCAT_ALIGNS}.gz
-check_exit_status "move, extract alignments" $?
 export ALIGNS_PART=chir_modeltest.partition
-cp ${TARGET}/${ALIGNS_PART} ./
-check_exit_status "move partitions" $?
+export ML_TREE_NAME=chir_ml.tree
+
+cp ${TARGET}/${CONCAT_ALIGNS}.gz ./ \
+    && gunzip ${CONCAT_ALIGNS}.gz \
+    && cp ${TARGET}/${ALIGNS_PART} ./ \
+    && cp ${TARGET}/${ML_TREE_NAME} ./
+check_exit_status "move + gunzip alignments, move partitions and ML tree" $?
 
 
 
-#' Note that `--extra thread-pin` was added to avoid the following error:
+#' This function runs bootstrapping with `--extra thread-pin` or
+#' `--extra thread-nopin`.
+#' These arguments are used to avoid the following error:
 #' "ERROR: CPU core oversubscription detected! RAxML-NG will terminate now
 #'  to avoid wasting resources."
-raxml-ng --bootstrap --msa ${CONCAT_ALIGNS} --prefix ${PREFIX} \
-    --bs-trees 100 \
-    --threads ${THREADS} \
-    --extra thread-pin \
-    --outgroup Mdomes \
-    --data-type aa \
-    --model ${ALIGNS_PART} \
-    --brlen scaled \
-    --seed ${THIS_SEED} \
-    1> >(tee -a ${PREFIX}.stdout)
-status=$?
+run_raxml_boot () {
+    if [ "$1" != "thread-pin" ] && [ "$1" != "thread-nopin" ]; then
+        echo "ERROR: invalid input to run_raxml_boot" 1>&2
+        echo "       Must be \"thread-pin\" or \"thread-nopin\", and" 1>&2
+        echo "       yours is \"$1\"." 1>&2
+    fi
+    #'
+    #' I'm making it perform the MRE-based bootstopping test every 50 replicates
+    #' and terminate once it surpasses the cutoff value of 0.01 (a strict value),
+    #' up to a maximum of 1000 replicates.
+    #'
+    raxml-ng --bootstrap --msa ${CONCAT_ALIGNS} --prefix ${PREFIX} \
+        --bs-trees autoMRE{1000} \
+        --threads ${THREADS} \
+        --workers auto{${THREADS}} \
+        --extra ${1} \
+        --bs-cutoff 0.01 \
+        --outgroup Mdomes \
+        --data-type aa \
+        --model ${ALIGNS_PART} \
+        --brlen scaled \
+        --seed 1425299936 \
+        | tee ${PREFIX}.stdout
+    local stts=$?
+    return $stts
+}
+
+run_raxml_boot "thread-pin"
+export status=$?
 
 
-#' Sometimes we still get the 'CPU core oversubscription' error by chance,
-#' so if that happens, we'll try it again with `--extra thread-nopin` to see
+#' If using `--extra thread-pin` produces the 'CPU core oversubscription' error,
+#' then we'll try it again with `--extra thread-nopin` to see
 #' if that fixes the problem.
 if [ "$status" != "0" ]; then
     if grep -Fq 'ERROR: CPU core oversubscription detected!' ${PREFIX}.raxml.log; then
         mv ${CONCAT_ALIGNS} ${ALIGNS_PART} ../ \
             && rm * \
             && mv ../${CONCAT_ALIGNS} ../${ALIGNS_PART} ./
-        raxml-ng --bootstrap --msa ${CONCAT_ALIGNS} --prefix ${PREFIX} \
-            --bs-trees 100 \
-            --threads ${THREADS} \
-            --extra thread-nopin \
-            --outgroup Mdomes \
-            --data-type aa \
-            --model ${ALIGNS_PART} \
-            --brlen scaled \
-            --seed ${THIS_SEED} \
-            1> >(tee -a ${PREFIX}.stdout)
-        check_exit_status "raxml-ng thread-nopin" $?
+        run_raxml_boot "thread-nopin"
+        check_exit_status "raxml-ng boot thread-nopin" $?
     else
-        check_exit_status "raxml-ng" $status
+        check_exit_status "raxml-ng boot" $status
     fi
+else
+    check_exit_status "raxml-ng boot" $status
 fi
 
 
 
+
+#' Now look at branch support
+mkdir chir_supp
+cd chir_supp
+raxml-ng --support \
+    --tree ../${ML_TREE_NAME} \
+    --bs-trees ../${PREFIX}.raxml.bootstraps \
+    --outgroup Mdomes \
+    --prefix chir_raxml_supp \
+    --threads auto{${THREADS}} \
+    --bs-metric fbp,tbe \
+    | tee chir_raxml_supp.stdout
+check_exit_status "raxml-ng support" $?
+cd ..
+
+
 # These are stored elsewhere:
-rm ${CONCAT_ALIGNS} ${ALIGNS_PART}
+rm ${CONCAT_ALIGNS} ${ALIGNS_PART} ${ML_TREE_NAME}
+
 
 cd ..
 tar -czf ${OUT_DIR}.tar.gz ${OUT_DIR}
 mv ${OUT_DIR}.tar.gz ${TARGET}/
 
 rm -r ${OUT_DIR}
-
 
 
